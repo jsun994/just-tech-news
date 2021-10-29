0f456b8da6c5a4685b6c7b2229f9aec7d4fcf3ed
jsun <jaysun054@gmail.com>
2021-10-29 14:46:05 -0700

    comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
         attributes: [
             'id',
@@ -12,7 +13,16 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
+        order: [['created_at', 'ASC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -39,25 +49,33 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
-                {
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User,
                 attributes: ['username']
-                }
-            ]
-        })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
             }
-            res.json(dbPostData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
-
+  
 router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
@@ -71,26 +89,25 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-
-// PUT /api/posts/upvote
+  
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+    Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
     });
 });
-
+  
 router.put('/:id', (req, res) => {
     Post.update(
         {
             title: req.body.title
         },
         {
-        where: {
-            id: req.params.id
+            where: {
+                id: req.params.id
             }
         }
     )
@@ -106,7 +123,7 @@ router.put('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
-
+  
 router.delete('/:id', (req, res) => {
     Post.destroy({
         where: {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Comment, Vote } = require('../../models');
 
-// GET /api/users
+
+// get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
     User.findAll({
         attributes: { exclude: ['password'] }
     })
@@ -13,15 +13,25 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-
-// GET /api/users/1
+  
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
         include: [
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
@@ -36,15 +46,14 @@ router.get('/:id', (req, res) => {
             res.status(404).json({ message: 'No user found with this id' });
             return;
         }
-            res.json(dbUserData);
-        })
+        res.json(dbUserData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
-
-// POST /api/users
+  
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
@@ -58,14 +67,15 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-
+  
 router.post('/login', (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
             email: req.body.email
         }
-    }).then(dbUserData => {
+    })
+    .then(dbUserData => {
         if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
@@ -74,15 +84,13 @@ router.post('/login', (req, res) => {
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
-            }
+        }
         res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 });
-
-// PUT /api/users/1
+  
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
         individualHooks: true,
@@ -102,8 +110,7 @@ router.put('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
-
-// DELETE /api/users/1
+  
 router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
@@ -122,5 +129,5 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
-
+  
 module.exports = router;

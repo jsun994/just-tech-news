a237faf7d944844f005aeab410bc34059b49b69b
jsun <jaysun054@gmail.com>
2021-11-02 17:56:31 -0700

    start

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const sess = {
 };
 
 app.use(session(sess));
+
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
@@ -29,6 +30,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: true }).then(() => {
